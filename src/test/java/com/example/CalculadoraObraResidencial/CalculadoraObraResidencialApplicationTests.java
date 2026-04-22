32d609b4e14448560ddd84dfaa5e71741d50e584
Daniel Godoi Alves Watrin <daniel.watrin@semmpreceub.com>
2026-04-22 14:47:18 -0300

    Primeiro commit definindo a estrutura do projeto!

---
+++ src/test/java/com/example/CalculadoraObraResidencial/CalculadoraObraResidencialApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.CalculadoraObraResidencial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraObraResidencialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
